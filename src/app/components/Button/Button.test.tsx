a9c391b625f5028014ee6f92213f7caa948ce462
Bordin Intarma <bordin@odds.team>
2020-08-01 12:56:59 +0700

    Modified structure

--- src/app/components/Button/Button.test.tsx
+++ src/app/components/Button/Button.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Box from './Box';
+import Button from './Button';
 
 it('It should mount', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Box />, div);
+  ReactDOM.render(<Button />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.scss';
+
+const Button: React.FC = () => (
+  <div className="Button">
+    Button Component
+  </div>
+);
+
+export default Button;
